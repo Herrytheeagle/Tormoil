3fa5a83b064ff39a62c9cbad5804e9d6b4dbdb8b
Heritage Samuel Falodun <herrytheeagle@gmail.com>
2020-09-11 11:53:20 -0500

    clrd

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

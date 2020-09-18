8629f90d43bed232d92b5edf42bff3d7143eb708
Heritage Samuel Falodun <herrytheeagle@gmail.com>
2020-09-18 17:20:47 -0500

    SnB

--- lib/app/sign_in/sign_in_button.dart
+++ lib/app/sign_in/sign_in_button.dart
@@ -1,8 +1,14 @@
+import 'dart:html';
+
 import 'package:Tormoil/app/sign_in/sign_in_button.dart';
 import 'package:Tormoil/common_widgets/custom_raised_button.dart';
+import 'package:flutter/material.dart';
 
 class SignInButton extends CustomRaisedButton {
   SignInButton({
-    
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
   })
 }
